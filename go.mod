130196ea47358132e20614ed3c026df25091eaca
Jon Ribeiro <contact@jonathas.com>
2023-07-08 19:01:02 +0200

    chore: initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jonathas/go-rest-api
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/jonathas/go-rest-api/routes"
+)
+
+func main() {
+	routes.HandleRequests()
+}
